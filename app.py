5a965f96a4ca494b4ef8326f4b3fa4d95919e70a
Sauce77 <sauceortediego505@gmail.com>
2025-05-07 09:19:52 -0600

    se agrego driver global

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
-from scripts.connections import connection_example, ingresar_login
+from flask import Flask, render_template, request, redirect, url_for
+from scripts.connections import iniciar_driver, connection_example, ingresar_login, obtener_componentes_caja
 app = Flask(__name__)
+
+# webdriver de la aplicacion
+app_driver = None
 
 @app.route('/')
 def root():
@@ -16,16 +19,28 @@ def login():
         username = request.form.get("username")
         password = request.form.get("password")
 
-        credencialesValidas,driver = ingresar_login(username,password)
+        # iniciamos el driver
+        app_driver, mensajes = iniciar_driver()
 
-        if credencialesValidas:
-            driver.quit()
-            return "Sesion iniciada con exito!"
+        if app_driver:
+            if ingresar_login(username,password):
+                return redirect(url_for('ingresar_caja'))
+            else:
+                app_driver.quit()
+                return "Credenciales incorrectas."
         else:
-            return "Credenciales incorrectas."
+            # si no se pudo iniciar el driver
+            return mensajes
 
 
     return render_template("login.html")
+
+@app.route('/caja', methods=["GET","POST"])
+def ingresar_caja():
+
+
+
+    return "ingresar caja"
 
 if __name__ == '__main__':
     app.run(port="9000",host="0.0.0.0",debug=True)
--- scripts/connections.py
+++ scripts/connections.py
@@ -19,7 +19,128 @@ from selenium.webdriver.chrome.service import Service
 
 url = "https://mujerhispana.maspunto.online"
 
+# ---------------------------- Driver -------------------------------
+
+def iniciar_driver():
+    """
+        Inicia las configuraciones del driver. Retorna el driver.
+    """
+    try:
+        options = webdriver.ChromeOptions()
+        options.add_argument("--start-maximized")
+        options.add_experimental_option("useAutomationExtension", False)
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option(
+            "prefs",
+            {
+                "credentials_enable_service": False,
+                "profile.password_manager_enabled": False,
+                "profile.default_content_setting_values.notifications": 2
+                # with 2 should disable notifications
+            },
+        )
+
+        options.add_experimental_option("prefs", {"profile.default_content_setting_values.notifications": 2}) 
+
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+        return driver, "Driver iniciado!"
+    except Exception as e:
+        return None, f"Ha ocurrido un error: {e}"
+
+# ------------------------------- Login ------------------------------
+
+def revisar_credenciales(driver, username, password):
+    """
+        Ingresa las credenciales para ingresar a home.
+        Se espera que se pueda ingresar a home hasta cierto tiempo
+        en caso de no lograrlo, se considera que las credenciales
+        son incorrectas o hubo un error.
+    """
+    try:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys(username)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys(password + Keys.RETURN)
+        # Esperar a que cargue el dashboard
+        WebDriverWait(driver, 20).until(EC.url_contains("/home"))
+        return True
+    except TimeoutException:
+        return False
+
+def ingresar_login(driver, username, password):
+    """
+        Recibe el username y la contrasena del usuario. Inicia sesion,
+        si es valido retorna True, de lo contrario,
+        retorna False y el valor del driver es None.
+    """
+    url_login = url + "/login"
+    driver.get(url_login)
+    
+    if revisar_credenciales(driver,username, password):
+        return True
+
+    else: 
+        driver.quit()
+        return False
+
+# ---------------------- Cajas --------------------------------------------
+def obtener_componentes_caja(driver):
+    """
+        Al iniciar sesion, se despliega un modal para la seleccion de 
+        la caja. Se obtienen los valores de los selectores.
+        En caso de no encontrar el modal ser retorna None.
+    """
+    # buscar el modal para seleccionar caja
+    modalCaja = encontrar_componentes.encontrarModalCaja(driver)
+
+    if modalCaja and modalCaja.is_displayed():
+
+        # almacenamos los valores de los selectores
+        valoresSelectores = {}
+
+        # encontramos el componente selector para sucursal
+        componenteSelectorSucursal = encontrar_componentes.encontrarComponenteID(modalCaja, "selectSucursal")
+
+        # si encuentra el selector sucursal
+        if componenteSelectorSucursal:
+
+            selectSucursal = Select(componenteSelectorSucursal)
+
+            for index, opcion_sucursal in enumerate(selectSucursal.options):
+
+                # almacenamos la opcion
+                valoresSelectores[f"{opcion_sucursal.text}"] = []
+
+                # seleccionamos la opcion actual
+                selectSucursal.select_by_index(index)
+
+                # encontramos el selector de caja con las opciones para esta sucursal
+                componenteSelectorCaja = encontrar_componentes.encontrarComponenteID(modalCaja, "selectCaja")
+
+                # si encuentra el selector caja
+                if componenteSelectorCaja:
+
+                    selectCaja = Select(componenteSelectorCaja)
+
+                    for opcion_caja in selectCaja:
+
+                        if opcion_caja.get_attribute("disabled"):
+                            # si la opcion no esta disponible
+                            continue
+
+                        # agregamos la opcion de caja a la variable de la sucursal
+                        valoresSelectores[f"{opcion_sucursal.text}"].append(opcion_caja.text)
+
+        return valoresSelectores
+
+    # no se encontro el modal para ingresar caja
+    return None
+
+# -------------------------- Ejemplo de conexion -----------------------------------
+    
 def connection_example(cadena):
+    """
+         Ejemplo de conexion. 
+    """
     # Configuración de opciones para el navegador (modo headless)
     options = Options()
     # options.add_argument('--headless')  # Ejecuta el navegador en segundo plano
@@ -47,100 +168,3 @@ def connection_example(cadena):
     driver.quit()
 
     return titulo
-
-def revisar_credenciales(driver,username,password):
-    """
-        Ingresa las credenciales para ingresar a home.
-        Se espera que se pueda ingresar a home hasta cierto tiempo
-        en caso de no lograrlo, se considera que las credenciales
-        son incorrectas o hubo un error.
-    """
-    try:
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys(username)
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys(password + Keys.RETURN)
-        # Esperar a que cargue el dashboard
-        WebDriverWait(driver, 20).until(EC.url_contains("/home"))
-        return True
-    except TimeoutException:
-        return False
-    
-def ingresar_pin(driver,pin):
-    """
-        Ingresa el pin para validar la caja, en caso de ser incorrecto
-    """
-
-def ingresar_caja(driver):
-    """
-        Al iniciar sesion, se despliega un modal para la seleccion de 
-        la caja. Se ingresa un pin y se seleccionan la caja disponible
-        para continuar.
-    """
-    # buscar el modal para seleccionar caja
-    modalCaja = encontrar_componentes.encontrarModalCaja(driver)
-
-    if modalCaja and modalCaja.is_displayed():
-
-        # encontramos el componente selector para sucursal
-        componenteSelectorSucursal = encontrar_componentes.encontrarComponenteID(modalCaja, "selectSucursal")
-
-        if componenteSelectorSucursal:
-            selectorSucursal = Select(componenteSelectorSucursal)
-            
-            selectorSucursal.select_by_index(2)
-            time.sleep(2)
-
-            # encontramos el componente selector para caja
-            componenteSelectorCaja = encontrar_componentes.encontrarComponenteID(modalCaja, "selectCaja")
-
-            if componenteSelectorCaja:
-                selectorCaja = Select(componenteSelectorCaja)
-            
-                selectorCaja.select_by_index(1)
-                time.sleep(2)
-
-
-
-            # driver.quit()
-            return "seleccionado!"
-        
-
-
-        else:
-            return "No se encontro selectorSucursal"
-
-    return "modal caja desplegado"
-
-def ingresar_login(username,password):
-    """
-        Recibe el username y la contrasena del usuario. Inicia sesion,
-        si es valido retorna True junto con el driver, de lo contrario,
-        retorna False y el valor del driver es None.
-    """
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    options.add_experimental_option("useAutomationExtension", False)
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option(
-        "prefs",
-        {
-            "credentials_enable_service": False,
-            "profile.password_manager_enabled": False,
-            "profile.default_content_setting_values.notifications": 2
-            # with 2 should disable notifications
-        },
-    )
-
-    options.add_experimental_option("prefs", {"profile.default_content_setting_values.notifications": 2}) 
-
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-    url_login = url + "/login"
-    driver.get(url_login)
-    
-    if revisar_credenciales(driver,username, password):
-        return True, driver
-
-    else: 
-        driver.quit()
-        return False, None
-    
